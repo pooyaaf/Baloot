2ad86d2f0e6add553280687747b0cab60623c03f
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-02-24 18:52:36 +0330

    Add GetCommodityByIdTest.

---
+++ src/test/java/Baloot/GetCommodityByIdTest.java
@@ -0,0 +1,62 @@
+package Baloot;
+
+import Baloot.Commands.GetCommodityById;
+import Baloot.Exception.CommodityNotFound;
+import Baloot.Exception.UserNotFound;
+import Baloot.Model.CommodityByIdModel;
+import Baloot.Model.ReportCommodityModel;
+import org.junit.Before;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GetCommodityByIdTest {
+    private Integer commodityId = 1;
+
+    public ReportCommodityModel generateReportCommodityModel(int id, String name, int providerId, double price, String categories, double rating) {
+        ReportCommodityModel reportCommodityModel = new ReportCommodityModel();
+        reportCommodityModel.id = id;
+        reportCommodityModel.name = name;
+        reportCommodityModel.providerId = providerId;
+        reportCommodityModel.price = price;
+        reportCommodityModel.categories = categories;
+        reportCommodityModel.rating = rating;
+        return reportCommodityModel;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // Generate providers
+        DataGenerator.GenerateProvider(1, "Provider A", "2023-02-23");
+        // Generate commodities
+        DataGenerator.GenerateCommodity(1, "Product A", 1, 10.0, "[Phone]", 4.0, 100);
+    }
+
+    @Test
+    public void getCommodityById_ShouldReturnExpectedCommodityInfo() throws CommodityNotFound, Exception {
+        ReportCommodityModel expectedModel = generateReportCommodityModel(1, "Product A", 1, 10.0, "[Phone]", 4.0);
+
+        CommodityByIdModel inputModel = new CommodityByIdModel();
+        inputModel.id = commodityId;
+
+        GetCommodityById command = new GetCommodityById();
+
+        ReportCommodityModel model = command.handle(inputModel);
+
+        assertThat(model)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedModel);
+    }
+
+    @Test(expected = CommodityNotFound.class)
+    public void getCommodityById_IfIdNotAvailableCommodityNotFound_Throws() throws Exception, CommodityNotFound {
+        ReportCommodityModel expectedModel = generateReportCommodityModel(1, "Product A", 1, 10.0, "[Phone]", 4.0);
+
+        CommodityByIdModel inputModel = new CommodityByIdModel();
+        Integer notAvailableCommodityId = 7;
+        inputModel.id = notAvailableCommodityId;
+
+        GetCommodityById command = new GetCommodityById();
+
+        command.handle(inputModel);
+    }
+}
