06f1e2ace898c6009fc311e9fcce9609deeb50e2
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-03-16 13:17:36 +0330

    Implement logic of addToBuyList.

---
+++ src/main/java/Baloot/Commands/AddCommodityToBuyList.java
@@ -0,0 +1,27 @@
+package Baloot.Commands;
+
+import Baloot.AcceptMethod;
+import Baloot.Context.ContextManager;
+import Baloot.Entity.Commodity;
+import Baloot.Entity.User;
+import Baloot.Exception.CommodityNotFound;
+import Baloot.Exception.CommodityNotInStuck;
+import Baloot.Exception.UserNotFound;
+import Baloot.Model.CommodityBuyListModel;
+import Baloot.Model.CommodityByIdModel;
+import Baloot.Model.view.CommodityShortModel;
+import Baloot.RequestMethod;
+import Baloot.Route;
+
+@Route("addToBuyList/{username}/{commodityId}")
+public class AddCommodityToBuyList extends Command{
+    @AcceptMethod(RequestMethod.GET)
+    public CommodityShortModel handle(CommodityBuyListModel input) throws Exception, CommodityNotFound, UserNotFound, CommodityNotInStuck {
+        Commodity commodity = ContextManager.getCommodity(Integer.parseInt(input.commodityId));
+        User user = ContextManager.getUser(input.username);
+        commodity.decreaseInStuck();
+        user.addToBuyList(commodity);
+
+        return commodity.getReportModel();
+    }
+}
---
+++ src/main/java/Baloot/Model/CommodityBuyListModel.java
@@ -0,0 +1,6 @@
+package Baloot.Model;
+
+public class CommodityBuyListModel {
+    public String username;
+    public String commodityId;
+}
