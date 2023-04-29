24cdc1a2262507bb2064e5ad707d70ecf2752f75
pooyaaf <pooya1380@gmail.com>
2023-04-29 21:35:41 +0430

    Add Global CORS

--- src/main/java/Baloot/BalootApplication.java
+++ src/main/java/Baloot/BalootApplication.java
@@ -2,6 +2,9 @@ package Baloot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BalootApplication {
@@ -9,5 +12,15 @@ public class BalootApplication {
     public static void main(String[] args) {
         SpringApplication.run(BalootApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
+            }
+        };
+    }
 }
