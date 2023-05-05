e667231b9a205276da6b303a1f529feadee4ff37
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-05-05 10:34:11 +0330

    Fix login controller status code.

--- src/main/java/Baloot/Controllers/LoginController.java
+++ src/main/java/Baloot/Controllers/LoginController.java
@@ -3,7 +3,9 @@ package Baloot.Controllers;
 import Baloot.Context.ContextManager;
 import Baloot.Context.UserContext;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @AllArgsConstructor
@@ -13,6 +15,8 @@ public class LoginController {
     public void login(@RequestParam("username") String username, @RequestParam("password") String password) {
         if (ContextManager.getInstance().isUserPassExist(username, password)) {
             UserContext.username = username;
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid username and password");
         }
     }
 }
