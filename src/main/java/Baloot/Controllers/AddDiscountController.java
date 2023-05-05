c40a26506c6d5baa4f84cef1a33b0ddb5962e1ac
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-05-06 00:28:17 +0330

    Fix discount controller.

--- src/main/java/Baloot/Controllers/AddDiscountController.java
+++ src/main/java/Baloot/Controllers/AddDiscountController.java
@@ -8,25 +8,24 @@ import Baloot.Exception.DiscountNotFound;
 import Baloot.Exception.ExpiredDiscount;
 import Baloot.Exception.UserNotAuthenticated;
 import Baloot.Exception.UserNotFound;
+import Baloot.Model.DiscountModel;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/addDiscount")
 public class AddDiscountController {
-    @PostMapping
-    public void addDiscount(@RequestParam("discount") String discount_amount) {
+    @GetMapping
+    public DiscountModel addDiscount(@RequestParam("discount") String discount_amount) {
         try {
             if (Authentication.isNotAuthenticated()) throw new UserNotAuthenticated();
             User user = ContextManager.getInstance().getUser(UserContext.username);
             Discount discount = ContextManager.getInstance().getDiscount(discount_amount);
             user.addDiscount(discount);
+            return discount.getModel();
         }
         catch (Exception | UserNotFound | DiscountNotFound | ExpiredDiscount | UserNotAuthenticated e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
