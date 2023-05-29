d8ae243ac64a3915ad4bc14ad322b85be5920db6
alireza <alirezaesmaeilzadeh113@gmail.com>
2023-05-29 15:17:28 +0330

    Add oauth.

--- src/main/java/Baloot/Controllers/LoginController.java
+++ src/main/java/Baloot/Controllers/LoginController.java
@@ -6,10 +6,7 @@ import Baloot.Entity.User;
 import Baloot.Model.UserModel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -25,7 +22,7 @@ import java.net.http.HttpResponse;
 import java.util.Date;
 import java.util.HashMap;
 
-class GithubUser {
+class   GithubUser {
     public String login;
     public String email;
     public Date createdAt;
@@ -35,61 +32,37 @@ class GithubUser {
 
 @RestController
 public class LoginController {
-    private String clientSecret = "0954082025c33039693fc39544446ae97893abfc";
-    private String clientId = "c2527d4cba3fd0dbd9c0";
+    private String clientSecret = "a0e7ed7a8da1513b7e3f57128d5d831f0cde78c1";
+    private String clientId = "d4723977604163f60960";
 
-    @GetMapping("/auth")
-    public ResponseEntity<String> auth(@RequestBody String code) {
-        String accessTokenString = String.format(
-                "https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientId,
-                clientSecret, code);
-
+    @GetMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String auth(@RequestParam(value = "code") String code) throws Exception {
+        String accessTokenURL = String.format(
+                "https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s",
+                clientId, clientSecret, code
+        );
         HttpClient client = HttpClient.newHttpClient();
-        URI accessTokenUri = URI.create(accessTokenString);
-        HttpRequest.Builder accessTokenBuilder = HttpRequest.newBuilder().uri(accessTokenUri);
+        URI accessTokenURI = URI.create(accessTokenURL);
+        HttpRequest.Builder accessTokenBuilder = HttpRequest.newBuilder().uri(accessTokenURI);
         HttpRequest accessTokenRequest = accessTokenBuilder.POST(HttpRequest.BodyPublishers.noBody())
-                .header("Accept", "Application/json").build();
-
-        HttpResponse<String> accessTokenResult = null;
-        try {
-            accessTokenResult = client.send(accessTokenRequest, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+                .header("Accept", "application/json").build();
+        HttpResponse<String> accessTokenResult = client.send(accessTokenRequest, HttpResponse.BodyHandlers.ofString());
         ObjectMapper mapper = new ObjectMapper();
-        HashMap<String, Object> resultBody = new HashMap<>();
-        try {
-            resultBody = mapper.readValue(accessTokenResult.body(), HashMap.class);
-        } catch (JsonProcessingException e1) {
-            e1.printStackTrace();
-        }
+        HashMap<String, Object> resultBody = mapper.readValue(accessTokenResult.body(), HashMap.class);
         String accessToken = (String) resultBody.get("access_token");
-
-        URI userDataUri = URI.create("https://api.github.com/user");
-        HttpRequest.Builder userDataBuilder = HttpRequest.newBuilder().uri(userDataUri);
+        URI userDateURI = URI.create("https://api.github.com/user");
+        HttpRequest.Builder userDataBuilder = HttpRequest.newBuilder().uri(userDateURI);
         HttpRequest req = userDataBuilder.GET().header("Authorization", String.format("token %s", accessToken)).build();
-        try {
-            HttpResponse<String> userDataResult = client.send(req, HttpResponse.BodyHandlers.ofString());
-            GithubUser user = mapper.readValue(userDataResult.body(), GithubUser.class);
-            UserModel model = new UserModel();
-            model.username = user.name;
-            model.email = user.email;
-            model.address = null;
-            model.credit = 0;
-            model.password=null;
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(user.createdAt);
-            calendar.add(Calendar.YEAR, -18);
-            model.birthDate = String.valueOf(calendar.getTime());
-            // TODO :  ResponseEntity.ok(JwtService.createToken(user.username))
-            return null;
+        HttpResponse<String> userDataResult = client.send(req, HttpResponse.BodyHandlers.ofString());
+        HashMap<String, Object> userData = mapper.readValue(userDataResult.body(), HashMap.class);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String email = (String) userData.get("email");
+        String name = (String) userData.get("name");
+        String nickname = (String) userData.get("login");
+        String birth_date = (String) userData.get("created_at");
+//        DataBase.getInstance().addUserWithGithub(email, null, name, nickname, birth_date);
 
-
-        return null;
+        return userDataResult.body();
     }
 
     @PostMapping("/login")
