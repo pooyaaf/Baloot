37acb39bba607e0d091e8b273b994869afd228a7
pooyaaf <pooya1380@gmail.com>
2023-05-29 17:22:19 +0330

    Add OAuth login database

--- src/main/java/Baloot/Controllers/LoginController.java
+++ src/main/java/Baloot/Controllers/LoginController.java
@@ -3,6 +3,7 @@ package Baloot.Controllers;
 import Baloot.Context.ContextManager;
 import Baloot.Context.UserContext;
 import Baloot.Entity.User;
+import Baloot.Exception.UserNotFound;
 import Baloot.Model.UserModel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.io.IOException;
 import java.net.URI;
@@ -32,8 +35,8 @@ class   GithubUser {
 
 @RestController
 public class LoginController {
-    private String clientSecret = "a0e7ed7a8da1513b7e3f57128d5d831f0cde78c1";
-    private String clientId = "d4723977604163f60960";
+    private String clientSecret = "fd22ab66840eac5dcd03624b27cca4051b915607";
+    private String clientId = "f3cc203f5df4b2e30d5c";
 
     @GetMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
     public String auth(@RequestParam(value = "code") String code) throws Exception {
@@ -55,12 +58,23 @@ public class LoginController {
         HttpRequest req = userDataBuilder.GET().header("Authorization", String.format("token %s", accessToken)).build();
         HttpResponse<String> userDataResult = client.send(req, HttpResponse.BodyHandlers.ofString());
         HashMap<String, Object> userData = mapper.readValue(userDataResult.body(), HashMap.class);
+        UserModel model = new UserModel();
+        model.password = null;
+        model.email = (String) userData.get("email");
+        model.username = (String) userData.get("login");
+        model.address = (String) userData.get("location");
 
-        String email = (String) userData.get("email");
-        String name = (String) userData.get("name");
-        String nickname = (String) userData.get("login");
-        String birth_date = (String) userData.get("created_at");
-//        DataBase.getInstance().addUserWithGithub(email, null, name, nickname, birth_date);
+        String createdAtString = (String) userData.get("created_at");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        Date createdAt = dateFormat.parse(createdAtString);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(createdAt);
+        calendar.add(Calendar.YEAR, -18);
+        model.birthDate = dateFormat.format(calendar.getTime());
+
+        //Database
+        ContextManager.getInstance().putUser(model.username, new User(model));
 
         return userDataResult.body();
     }
